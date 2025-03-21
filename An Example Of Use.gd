9cb9eb7051d2c2ee7d24030a692f460070fa6a79
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-03-20 22:13:14 -0300

    Updated The Example

--- An Example Of Use.gd
+++ An Example Of Use.gd
@@ -1,11 +1,9 @@
-# An Example Of Use
-
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
-@export var Corner_Correction_Pixels : int = 10
+@export var Corner_Correction_Pixels : int = 20
 
 func _Apply_Corner_Correction(delta: float) -> void:
 	var move_vec = Vector2(0, velocity.y * delta)
@@ -13,15 +11,14 @@ func _Apply_Corner_Correction(delta: float) -> void:
 	if velocity.y >= 0 or not test_move(global_transform, move_vec):
 		return
 	
-	for i in range(1, Corner_Correction_Pixels + 1):
-		var offset_x = i / 2.0
+	for i in range(1, Corner_Correction_Pixels):
 		for dir in [-1, 1]:
-			var offset = Vector2(offset_x * dir, 0)
-			if not test_move(global_transform.translated(offset), move_vec):
-				translate(offset)
+			if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), move_vec):
+				translate(Vector2(i / 2.0 * dir, 0))
 				if velocity.x * dir < 0:
 					velocity.x = 0
 				return
+
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
