aafce1364ed9fd57d427c470945f6955cc28fa75
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-03-20 21:39:56 -0300

    Update Corner-Correction.gd

--- Corner-Correction.gd
+++ Corner-Correction.gd
@@ -1,12 +1,17 @@
 var Corner_Correction_Pixels : int = 8
 
 func _Apply_Corner_Correction(delta: float) -> void:
-	if velocity.y < 0 and test_move(global_transform, Vector2(0, velocity.y * delta)):
-		for i in range(1, Corner_Correction_Pixels * 2 + 1):
-			for dir in [-1.0, 1.0]:
-				var offset = Vector2((i * dir) / 2, 0)
-				if not test_move(global_transform.translated(offset), Vector2(0, velocity.y * delta)):
-					translate(offset)
-					if velocity.x * dir < 0:
-						velocity.x = 0
-					return
+	var move_vec = Vector2(0, velocity.y * delta)
+	
+	if velocity.y >= 0 or not test_move(global_transform, move_vec):
+		return
+	
+	for i in range(1, Corner_Correction_Pixels + 1):
+		var offset_x = i / 2.0
+		for dir in [-1, 1]:
+			var offset = Vector2(offset_x * dir, 0)
+			if not test_move(global_transform.translated(offset), move_vec):
+				translate(offset)
+				if velocity.x * dir < 0:
+					velocity.x = 0
+				return
