846ba9aec3ab1f1a24cab17ae37ae5ad743142ed
SuperBenjaBros <PacoTieneUnGranToto@Gmail.com>
2025-03-26 16:52:41 -0300

    Add Identation To The Corner Correction Code

--- Corner-Correction.gd
+++ Corner-Correction.gd
@@ -1,15 +1,15 @@
-@export var Corner_Correction_Pixels : int = 20
+@export var Corner_Correction_Pixels: int = 20
 
 func _Apply_Corner_Correction(delta: float) -> void:
-var move_vec = Vector2(0, velocity.y * delta)
+    var move_vec = Vector2(0, velocity.y * delta)
 
-if velocity.y >= 0 or not test_move(global_transform, move_vec):
-return
+    if velocity.y >= 0 or not test_move(global_transform, move_vec):
+        return
 
-for i in range(1, Corner_Correction_Pixels):
-for dir in [-1, 1]:
-if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), move_vec):
-translate(Vector2(i / 2.0 * dir, 0))
-if velocity.x * dir < 0:
-velocity.x = 0
-return
+    for i in range(1, Corner_Correction_Pixels):
+        for dir in [-1, 1]:
+            if not test_move(global_transform.translated(Vector2(i / 2.0 * dir, 0)), move_vec):
+                translate(Vector2(i / 2.0 * dir, 0))
+                if velocity.x * dir < 0:
+                    velocity.x = 0
+                return
